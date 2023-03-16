3957a66415823ad7c60db21e724ec7e9e59ea350
oussamah <oussamah.jaber@gmail.com>
2023-03-16 10:14:19 +0100

    erreur

--- pages/troisieme-page.tsx
+++ pages/troisieme-page.tsx
@@ -5,14 +5,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      <div
-        onClick={() => {
-          console.log("hello");
-        }}
-        className="text-red-500 text-9xl md:text-blue-500 lg:text-green-500"
-      >
-        Deuxième page
-      </div>
       <div>Liens</div>
       <div className="flex flex-col lg:flex-row">
         <div>Bonjour les amis </div>
