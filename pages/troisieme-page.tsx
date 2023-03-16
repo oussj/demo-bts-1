c86d44f317ab50841c06ab37cced6399afa40b90
oussamah <oussamah.jaber@gmail.com>
2023-03-16 10:15:41 +0100

    erreur 2

--- pages/troisieme-page.tsx
+++ pages/troisieme-page.tsx
@@ -5,7 +5,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      <div>Liens</div>
       <div className="flex flex-col lg:flex-row">
         <div>Bonjour les amis </div>
         <div>Bonjour les amis </div>
